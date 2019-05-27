b0b70e2f124f6b2c3a1bb5d1b089ed84fe7e678e
Marc Neander <marcneander@Marcs-Air.lan>
2019-05-27 21:02:39 +0200

    Small changes to layout

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -15,24 +15,6 @@ const Background = () => {
                     className="svg"
                 >
                     <path
-                        fill="#638A52"
-                        className="st0"
-                        d="M685.6,38.8C418.7-11.1,170.2,9.9,0,30v96h1440V30C1252.7,52.2,1010,99.4,685.6,38.8z"
-                    />
-                </svg>
-            </div>
-            <div className="grass-light">
-                <svg
-                    version="1.1"
-                    id="Layer_1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    viewBox="0 0 1440 126"
-                    xmlSpace="preserve"
-                    className="svg"
-                >
-                    <path
                         fill="#467346"
                         className="st0"
                         d="M685.6,38.8C418.7-11.1,170.2,9.9,0,30v96h1440V30C1252.7,52.2,1010,99.4,685.6,38.8z"
